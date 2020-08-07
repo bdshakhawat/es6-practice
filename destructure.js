571cde72173c5cfb89ba1f84d0fe858e9824c7be
bdshakhawat <mdshakwat.hossain@gmail.com>
2020-08-07 07:46:33 -0700

    fourth commit

---
+++ destructure.js
@@ -0,0 +1,4 @@
+const person = {name:"sumon", phone: 01714, age: 42,};
+const {phone, age} = person;
+const name = person.name;
+console.log(name, phone,age);
---
+++ true-false.js
@@ -0,0 +1,11 @@
+// Falsy value
+// 0 , "", null, undefined, NaN,false.
+// Truthy Value
+// "0" , " " , [],{},any number or string
+const name = "Soliman"
+if (name.length > 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
