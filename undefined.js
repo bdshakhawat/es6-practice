09aa3acc4b06a3283cb63f53437efc0c995630dc
bdshakhawat <mdshakwat.hossain@gmail.com>
2020-08-07 08:10:12 -0700

    fifth commit

---
+++ undefined.js
@@ -0,0 +1,24 @@
+// let pakhi;
+// function add(num1 , num2){
+//     const sum = num1 + num2;
+//     return
+// }
+// const result = add(5,8);
+// console.log(result);
+// function add(num1 , num2){
+//         console.log(num1, num2);
+//     }
+//     const result = add(5);
+//     console.log(result);
+// console.log(pakhi);
+const darling = {name: "Nova", phone: 0167, class: "Eleven"};
+console.log(darling.gfName);
+let ages = [5,8,9,10];
+console.log(ages[15]);
+
+
+
+
+
+
+
